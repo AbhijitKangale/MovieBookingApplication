e6c94851dd4a034993b961e096ae7ce3f58158b4
mygithub2015 <manishyadav1990@outlook.com>
2019-11-25 19:44:09 +0530

    zuul configurations

--- bookmymovie-zuul/src/main/java/com/publicissapient/bookmymovie/zuul/BookMyMovieZuulApplication.java
+++ bookmymovie-zuul/src/main/java/com/publicissapient/bookmymovie/zuul/BookMyMovieZuulApplication.java
@@ -3,13 +3,15 @@ package com.publicissapient.bookmymovie.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class BookmymovieZuulApplication {
+@EnableZuulProxy
+public class BookMyMovieZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmymovieZuulApplication.class, args);
+		SpringApplication.run(BookMyMovieZuulApplication.class, args);
 	}
 
 }
