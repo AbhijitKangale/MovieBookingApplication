14c2ec0ebce6042740bfe3bd2b88a95f10f71c94
mygithub2015 <manishyadav1990@outlook.com>
2019-11-25 19:59:31 +0530

    eureka-server minute changes

--- bookmymovie-eureka/src/main/java/com/publicissapient/bookmymovie/eureka/BookMyMovieEurekaApplication.java
+++ bookmymovie-eureka/src/main/java/com/publicissapient/bookmymovie/eureka/BookMyMovieEurekaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class BookmymovieEurekaApplication {
+public class BookMyMovieEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmymovieEurekaApplication.class, args);
+		SpringApplication.run(BookMyMovieEurekaApplication.class, args);
 	}
 
 }
